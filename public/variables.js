b35599b698a95c501c45f3a760d5f1cc3aca1cd1
chioarenillas <arenillasr@gmail.com>
2025-09-29 18:29:57 +0200

    changes

---
+++ public/variables.js
@@ -0,0 +1,47 @@
+// tipo de dato string
+var nombre = "chio";
+console.log(nombre);
+
+// dato number
+var number = 1000
+console.log(number);
+
+//dato object
+var object = {
+    nombre : "chio",
+    apellido : "alvarez",
+    telefono : "858903"
+} 
+console.log(object);
+console.log(typeof object)
+
+//dato boolean (T, F)
+var bandera = true;
+console.log(bandera);
+console.log(typeof bandera);
+
+//dato function
+function myFunction(){}
+console.log(myFunction);
+
+//dato symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+
+//Tipo clase es una function
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona);
+
+//tipo undefined
+var x;
+console.log(x);
+
+//null = ausencia de valor (es una var de tipo object)
+var y = null;
+console.log(y);
+console.log(typeof y);
