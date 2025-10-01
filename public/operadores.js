ca3d532c246b9b23b76a756d00ca50bfa3326f20
chioarenillas <arenillasr@gmail.com>
2025-10-01 11:27:02 +0200

    changes

--- public/operadores.js
+++ public/operadores.js
@@ -1,5 +1,7 @@
 let a = 3, b = 2;
 let z = a + b;
+
+//Operaciones básicas
 console.log("Resultado de la suma: " + z );
 
 z = a - b;
@@ -17,6 +19,16 @@ console.log("Resultado de la residuo: " + z );
 z = a ** b; // elevado a 
 console.log("Resultado de operador exponente: " + z );
 
+//Incremento
+// Pre-incremento (el operador ++ antes de la variable, se incrementa en el momento)
+z = ++a;
+console.log(a);
+console.log(z);
+
+//Post-incremento (el operador ++ despues de la variable) (se incrementa no en el momento en que lo usas, sino la siguiente vez que se usa esa variable, la prox vez que se use estará incrementada)
+z = b++;
+console.log(b);
+console.log(z);
 
 
 
